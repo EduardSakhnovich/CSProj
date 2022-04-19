f2704945a98771c95b09d9bca015e8579d42616e
EduardSakhnovich <edisakhn12@gmail.com>
2022-04-19 12:21:43 +0300

    Add project files.

---
+++ CSProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
